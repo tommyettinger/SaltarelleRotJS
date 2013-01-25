37c2b8d0125f8f036abce2c24cb4914611e73a23
Tommy Ettinger <tommy.ettinger@gmail.com>
2013-01-24 22:29:36 -0800

    Modifying jQuery plugin, probably screwed up.

---
+++ RotJS/RotDisplay.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Html;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Xml;
+
+namespace RotJS
+{
+
+    [Imported]
+    [IgnoreNamespace]
+    [ScriptName("Display")]
+    public class ROTDisplay
+    {
+        public ROTDisplay(JsDictionary options)
+        {
+        }
+        public static void MyMethod()
+        {
+        }
+    }
+}
