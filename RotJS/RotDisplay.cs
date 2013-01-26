3584b4e12eeba37b7b85b2cb9038da87f72f9a63
Tommy Ettinger <tommy.ettinger@gmail.com>
2013-01-26 02:05:29 -0800

    Thank you, Erik and Ondrej!  It works!

--- RotJS/RotDisplay.cs
+++ RotJS/RotDisplay.cs
@@ -11,8 +11,44 @@ using System.Xml;
 namespace ROT
 {
 
+    [Imported, Serializable]
+    public class DisplayOptions
+    {
+        /*
+         * 
+ * @param {int} [options.height=ROT.DEFAULT_HEIGHT]
+ * @param {int} [options.fontSize=15]
+ * @param {string} [options.fontFamily="monospace"]
+ * @param {string} [options.fontStyle=""] bold/italic/none/both
+ * @param {string} [options.fg="#ccc"]
+ * @param {string} [options.bg="#000"]
+ * @param {int} [options.fps=25]
+ * @param {float} [options.spacing=1]
+ * @param {string} [options.layout="rect"]
+         */
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Fps { get; set; }
+        public int FontSize { get; set; }
+        public string FontFamily { get; set; }
+        public string FontStyle { get; set; }
+        [PreserveCase]
+        public string fg { get; set; }
+        [PreserveCase]
+        public string bg { get; set; }
+        public float Spacing { get; set; }
+        public string Layout { get; set; }
+        public DisplayOptions() { }
+        public DisplayOptions(int width, int height) { }
+        public DisplayOptions(int width, int height, string fontFamily) { }
+        public DisplayOptions(int width, int height, string fontFamily, string fontStyle) { }
+        public DisplayOptions(int width, int height, string fontFamily, string fontStyle, string fg, string bg) { }
+        public DisplayOptions(int width, int height, string fontFamily, string fontStyle, string fg, string bg, int fps) { }
+        public DisplayOptions(int width, int height, string fontFamily, string fontStyle, string fg, string bg, int fps, float spacing) { }
+        public DisplayOptions(int width, int height, string fontFamily, string fontStyle, string fg, string bg, int fps, float spacing, string layout) { }
+    }
+
     [Imported]
-    [IgnoreNamespace]
     [PreserveMemberCase]
     public class Display
     {
@@ -20,10 +56,9 @@ namespace ROT
         /// Fiddling with this, unsure what it needs.
         /// </summary>
         /// <param name="options"></param>
-        [InlineCode("ROT.Display({*options})")]
-        public Display(params object[] options)
-        {
-        }
+
+        public Display(DisplayOptions options) { }
+    
         [PreserveCase]
         public void DEBUG(int x, int y, int what)
         {
